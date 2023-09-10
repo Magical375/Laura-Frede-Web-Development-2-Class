9f7b883b82011c89fb9e22cd2f15b8d3c0dad18c
Magical375 <100384587+Magical375@users.noreply.github.com>
2023-09-09 21:10:05 -0600

    more things to save

---
+++ Files From Web Development 1/script for a colourful website.js
@@ -0,0 +1,24 @@
+/*
+userInput1 = document.getElementById('num1').value;
+x = parseInt(userInput1);
+userInput2 = document.getElementById('num2').value;
+y = parseInt(userInput2);
+
+function add(text1,text2)
+{
+    x = parseInt(text1);
+    y = parseInt(text2);
+    return x+y;
+}
+*/
+
+function changeColor()
+{
+document.getElementById("main").style.backgroundColor=
+document.getElementById("pickcolor").value;
+}
+
+function changeLoadColor()
+{
+document.getElementById('main').style.backgroundColor=rgb(0,255,242);
+}
