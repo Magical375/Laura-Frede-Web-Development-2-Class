1d26af59a5dfb272011d2aa47424a7e2579aa8eb
Magical375 <100384587+Magical375@users.noreply.github.com>
2023-09-14 16:38:10 -0600

    adding fuction to app.js {adding fucking comments} this is a text suggestion

--- 14 September 2023/app.js
+++ 14 September 2023/app.js
@@ -7,3 +7,10 @@ console.log(greeting+" "+name);
 console.log(z);
 //apparently you need to have a good commit message so later you can go 
 //back and understand what you did
+
+let b = 10;
+let g =8;
+function addEventListener(b,g)
+{
+    return b+g;
+}
