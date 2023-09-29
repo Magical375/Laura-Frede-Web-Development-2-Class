146e87f88dd3ba0d44d96484e76f5ec24919f068
Magical375 <100384587+Magical375@users.noreply.github.com>
2023-09-28 18:00:34 -0600

    changed StudentInfo and page

--- cprg306-assignments/app/StudentInfo.js
+++ cprg306-assignments/app/StudentInfo.js
@@ -2,21 +2,13 @@
 export default function Studentinfo() {
     return (
       <div>
-        <MyHeading />
+       
         Student Info
-        <MyParagraph />
+        <link href="https://github.com/Magical375/cprg306-assignments">Git Hub Repository</link>
         name = Laura Frede 
         course = CPRG 306
       </div>
     );
   }
-  export default function Navigation() {
-    return (
-    <nav>
-      <Link href="https://github.com/Magical375/cprg306-assignments">Git Hub Repository</Link>
-    </nav>
-    );
-  }
- 
 
 
