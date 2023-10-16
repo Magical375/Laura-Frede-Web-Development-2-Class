ed66be5f6494fe31599ff651cb959c39883c7905
Magical375 <100384587+Magical375@users.noreply.github.com>
2023-10-16 11:05:40 -0600

    created new files, moved some files

--- cprg306-assignments/app/page.js
+++ cprg306-assignments/app/page.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Link from "next/link";
-import Studentinfo from './StudentInfo';
+import Studentinfo from './week2/StudentInfo';
 export default function Home() {
   return (
     <Studentinfo></Studentinfo>
