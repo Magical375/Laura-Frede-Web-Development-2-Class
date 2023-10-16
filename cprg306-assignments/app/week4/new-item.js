ece9cab1959b8e61c98de3306e7ba815e5ec734e
Magical375 <100384587+Magical375@users.noreply.github.com>
2023-10-16 14:32:48 -0600

    working on week 4

--- cprg306-assignments/app/week4/new-item.js
+++ cprg306-assignments/app/week4/new-item.js
@@ -2,24 +2,57 @@
 
 import { useState } from "react";
 
-const [name, setName] = useState("");
-const [quantity, setQuantity] = useState("1");
-const [category, setCategory] = useState("produce");
-const submitFunction = (event) => {
-    event.preventDefault();
-    // Handle form data
-  };
+export default function newItem() {
+    const [name, setName] = useState("");
+    /*const [quantity, setQuantity] = parseString(quantity, setQuantity);
+    [quantity, setQuantity] = useState("1");*/
+    const [quantity, setQuantity] = useState(1);
+    const [category, setCategory] = useState("produce");
+  
+  useState("1");
+    const submitFunction = (event) => {
+        event.preventDefault();
+        // Handle form data
+    
+    const newItem = {
+        name,
+        quantity,
+        category,
+    };
+    console.log(newItem);
 
+    setItemCreated(true);
 
-export default function NewItem() {
+    setName("");
+    setQuantity("1");
+    setCategory("produce");
+
+    setItemCreated(false);
+    };  
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
+
+    const handleQuantityChange = (event) => {
+        setQuantity(event.target.value);
+    };
+    
+    const handleCategoryChange = (event) => {   
+        setCategory(event.target.value);
+    };
+
     return(       
         <main>
-        
+            {itemCreated && <p>Item created!</p>}
+        <h1>Create New Item</h1>
+
         <form onSubmit="submitFunction(event)">
-          <input type="text" name="name" placeholder="Enter the item name" />
-          <input type="number" name="quantity" placeholder="Enter the quantity of the item" />
-          <input type="text" name="category" placeholder="Enter the category of the item" />
-          <button type="submit">Submit</button>
+        Enter the item name: <input type="text" required onChange={setName}  value={name}/>
+        Enter the quantity of the item: <input type="number" min={1} max={99} required onChange={setQuantity}  value={quantity}/>
+        Enter the category of the item: <input type="text" required onChange={setCategory}  value={category}/>  
+       
+        <button type="submit">Create Item</button>
         </form>
         
         <NewItem/>
